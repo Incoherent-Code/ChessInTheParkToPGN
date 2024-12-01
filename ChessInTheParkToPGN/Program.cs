b85fbb197144bd52e1d8c4455036f7da04d1b80f
Incoherent-Code <aiaub1212@gmail.com>
2024-11-30 22:44:45 -0600

    Added support for multiple gifs

--- ChessInTheParkToPGN/Program.cs
+++ ChessInTheParkToPGN/Program.cs
@@ -1,11 +1,32 @@
-﻿namespace ChessInTheParkToPGN {
+﻿using System.Diagnostics;
+
+namespace ChessInTheParkToPGN {
    internal class Program {
       static void Main(string[] args) {
-         if (args.Length == 0 || !File.Exists(args[0])) {
-            Console.WriteLine("Please start the analyzer with a valid file path!");
+         if (args.Length == 0) {
+            ErrorMessage("Please start the analyzer with a valid file path!");
             Environment.Exit(1);
          }
-
+         foreach (string arg in args) {
+            if (!File.Exists(arg)) {
+               ErrorMessage($"Invalid file Path: {arg}");
+               continue;
+            }
+            try {
+               var game = new ChessGame(arg);
+               game.saveToFile(Path.Combine(Directory.GetCurrentDirectory(), Path.GetFileNameWithoutExtension(arg)) + ".pgn");
+            }
+            catch (Exception ex) {
+               ErrorMessage(ex.Message);
+               if (Debugger.IsAttached)
+                  throw;
+            }
+         }
+      }
+      public static void ErrorMessage(string message) {
+         Console.ForegroundColor = ConsoleColor.Red;
+         Console.WriteLine(message);
+         Console.ForegroundColor = ConsoleColor.White;
       }
    }
 }
